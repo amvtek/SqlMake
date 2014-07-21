cfd677c02f419aa8f55f52f89f86940d2c70998d
amvtek <devel@amvtek.com>
2014-07-21 16:55:28 +0300

    bumping version to 0.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='SqlMake',
-    version='0.2.1',
+    version='0.2.2',
     author='AmvTek developers',
     author_email='devel@amvtek.com',
     packages=['sqlmake'],
